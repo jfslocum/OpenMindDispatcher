1104d93706c21cc3ed0f4cb610eee7ce25c574db
Joshua Slocum <jfslocum@gmail.com>
2014-09-10 12:04:21 -0400

    file to generate matlab worklist

---
+++ generateWorkList.py
@@ -0,0 +1,29 @@
+#!/cm/shared/openmind/anaconda/1.9.2/bin/python
+import sys
+
+neuronTypes = ['dormant, unconnected', 'dormant, excited', 
+               'moderate, inhibited', 'moderate, unconnected', 
+               'moderate, excited', 'active, inhibited', 
+               'active, unconnected']
+
+noise_coeffs = [5, 10, 20, 30]
+
+
+OUTPUT_FOLDER = "/om/user/jslocum/hmm2dlearning/hmmtrainresults"
+MATLAB_CALL = "matlab -singleCompThread -nojvm -nodisplay -nosplash -r runTrials('%s', '%s', '%s', '%s', 100, %i, 'modelChange', 800, '%s'"
+
+def writeWorklistToFile(f):
+    for truth_idx in range(pow(len(neuronTypes), 2)):
+        true_type1 = neuronTypes[truth_idx % len(neuronTypes)]
+        true_type2 = neuronTypes[truth_idx / len(neuronTypes)]
+        for guess_idx in range(pow(len(neuronTypes), 2)):
+            guess_type1 = neuronTypes[guess_idx % len(neuronTypes)]
+            guess_type2 = neuronTypes[guess_idx / len(neuronTypes)]
+            for n in range(len(noise_coeffs)):
+                noise = noise_coeffs[n]
+                f.write(MATLAB_CALL % (true_type1, true_type2,
+                                       guess_type1, guess_type2, noise, OUTPUT_FOLDER))
+
+
+with open(sys.argv[1]) as f:
+    writeWorklistToFile(f)
