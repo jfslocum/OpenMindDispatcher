61268f181c0eb4b8fb5f44f859739980f674ca09
jfslocum <jfslocum@gmail.com>
2015-03-25 15:44:05 -0400

    added example dispatch command

--- dispatch.sh
+++ dispatch.sh
@@ -5,3 +5,6 @@ module add mit/matlab/2014a #MATLAB
 echo "Number of cpu's is" $((SLURM_CPUS_PER_TASK * SLURM_JOB_NUM_NODES))
 mpiexec.hydra -bootstrap slurm -n $((SLURM_CPUS_PER_TASK * SLURM_JOB_NUM_NODES)) ./work_handler.py $1 $2
 
+#example invocation: $sbatch --cpus-per-task=12 -N2 dispatch.sh testing/openmindAnnealingShortTest.txt 
+#uses 24 cpus (12 cpus * 2 Nodes) to accomplish the work-items in openmindAnnealindShortTest.txt
+
